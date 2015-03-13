5e0f45d1af213cb70749eab201157f87812fdc7c
ikeda <ikeda@000196-M3.local>
2015-03-13 20:14:54 +0900

    起動スクリプト

---
+++ niku/scripts/run.sh
@@ -0,0 +1,5 @@
+nohup python manage.py genetic_single > /tmp/genetic1.log 2>&1 &
+nohup python manage.py genetic_single > /tmp/genetic2.log 2>&1 &
+nohup python manage.py genetic_single > /tmp/genetic3.log 2>&1 &
+nohup python manage.py genetic_single > /tmp/genetic4.log 2>&1 &
+
