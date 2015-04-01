9ede5955971f1958c2f53a28484f64c6c495dd9f
ikeda <ikeda@000196-M3.local>
2015-04-01 19:52:52 +0900

    動的に動作するよう修正

--- niku/apps/web/views.py
+++ niku/apps/web/views.py
@@ -2,10 +2,18 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from ..base.views import BaseView
+from module.oanda.constants import OandaAPIMode
+from module.oanda.models.api_account import AccountAPI
 
 
 class IndexView(BaseView):
+    """
+    アカウント情報のサマリーページ
+    """
     template_name = "web/index.html"
 
     def get(self, request, *args, **kwargs):
-        return self.render_to_response({})
+        account = AccountAPI(OandaAPIMode.PRODUCTION, 6181277).get_all()
+        return self.render_to_response({
+            'account': account,
+        })
